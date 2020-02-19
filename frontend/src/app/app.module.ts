4c1f17b4bdc2490c031ac9c3a0044d10942559a0
Cherry Goh <cherrycecegoh@gmail.com>
2020-02-20 05:22:02 +0800

    Work on top bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
