195e38551415ac5aedf5b8baf76c4d227bcb56bb
Cherry Goh <cherrycecegoh@gmail.com>
2020-02-27 00:07:56 +0800

    remove test files

--- frontend/src/app/shared/mocks/mock-entries.ts
+++ frontend/src/app/shared/mocks/mock-entries.ts
@@ -4,5 +4,10 @@ export const MOCK_ENTRIES: DiaryEntry[] = [
     { diaryEntryId: 1, diaryEntryName: 'Curry', 
     diaryEntryCalories: 600, diaryEntryCarbs: 100, 
     diaryEntryProtein: 60, diaryEntryFat: 60, 
-    username: 'johnsmith', mealId: 1, diaryEntryDate: new Date() }
+    username: 'johnsmith', mealId: 1, diaryEntryDate: new Date() },
+
+    { diaryEntryId: 2, diaryEntryName: 'Chicken Nuggets', 
+    diaryEntryCalories: 400, diaryEntryCarbs: 150, 
+    diaryEntryProtein: 20, diaryEntryFat: 40, 
+    username: 'johnsmith', mealId: 2, diaryEntryDate: new Date() }
 ]
